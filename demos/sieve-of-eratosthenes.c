0666ef65fea3ab08b1df39beb0ef0a5fa6f41fc8
Eric Herman <eric@freesa.org>
2017-02-19 11:41:02 +0000

    LGPLv3+ boiler-plate

--- demos/sieve-of-eratosthenes.c
+++ demos/sieve-of-eratosthenes.c
@@ -1,3 +1,18 @@
+/*
+sieve-of-eratosthenes.c: demo of using libeba embedable bit array
+Copyright (C) 2017 Eric Herman <eric@freesa.org>
+
+This work is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This work is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
--- src/eba.c
+++ src/eba.c
@@ -1,3 +1,18 @@
+/*
+eba.c: embedable bit array - hopefully somewhat suitable for small CPUs
+Copyright (C) 2017 Eric Herman <eric@freesa.org>
+
+This work is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This work is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+*/
+
 #include "eba.h"
 #include "eba-log.h"
 
